700e57256801ff06d1e200cbb60a693c111db833
Amirhossein Izadi <AmirIzadi2002@gmail.com>
2023-12-27 10:14:26 +0330

    veiws.py and 0001_initial.py refactor due to inhance readablily

--- MyBuildingManager/ticketing/views.py
+++ MyBuildingManager/ticketing/views.py
@@ -1,79 +1,60 @@
-from rest_framework.request import Request
-from rest_framework.permissions import IsAuthenticated,AllowAny, IsAuthenticatedOrReadOnly
-from rest_framework.decorators import api_view, permission_classes
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
-from .models import Ticket,TicketReply, Announcement
-from .serializers import TicketSerializer,TicketReplySerializer, AnnouncementSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Ticket, TicketReply, Announcement
+from .serializers import TicketSerializer, TicketReplySerializer, AnnouncementSerializer
 from myauth.permissions import IsAdmin
-from myauth.models import Profile
 
-SAFE_METHODS = ('GET', 'HEAD', 'OPTIONS')
-NOT_SAFE_METHODS = ('POST', 'PUT', 'PATCH','DELETE')
+# Utility to handle permission logic
+def get_custom_permissions(request_method):
+    if request_method in ('GET', 'HEAD', 'OPTIONS'):
+        return [AllowAny()]
+    else:
+        return [IsAdmin(), IsAuthenticated()]
 
-#Tickets APIs ---------------------------------------------------------------------
-class TicketListCreateView(generics.ListCreateAPIView):
+# Mixin for common create behavior
+class PerformCreateMixin:
+    def perform_create(self, serializer):
+        user_profile = get_object_or_404(Profile, user_id=self.request.user.id)
+        serializer.save(author=user_profile)
+
+# Tickets APIs ---------------------------------------------------------------------
+class TicketListCreateView(PerformCreateMixin, generics.ListCreateAPIView):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, ]
-
-    def perform_create(self, serializer):
-        try:
-            serializer.save(author=Profile.objects.get(user_id=self.request.user.id))
-        except:
-            return
-        
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class TicketRetrieveUpdateView(generics.RetrieveUpdateAPIView):
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated]
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
-#----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
 
-
-#Replies APIs ---------------------------------------------------------------------
-class ReplyListCreateView(generics.ListCreateAPIView):
+# Replies APIs ---------------------------------------------------------------------
+class ReplyListCreateView(PerformCreateMixin, generics.ListCreateAPIView):
     queryset = TicketReply.objects.all()
     serializer_class = TicketReplySerializer
-    permission_classes = [AllowAny,IsAdmin, IsAuthenticated ]
 
-    def perform_create(self, serializer):
-        try:
-            reply = serializer.save()
-            TicketReply.update(id=reply.parent_ticket.id)
-        except:
-            return
-    
     def get_permissions(self):
-        if self.request.method in SAFE_METHODS:
-            return [AllowAny(),]
-        elif self.request.method in NOT_SAFE_METHODS:
-            return  [IsAdmin(), IsAuthenticated() ]
+        return get_custom_permissions(self.request.method)
 
 class ReplyRetrieveUpdateView(generics.RetrieveUpdateAPIView):
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated]
     queryset = TicketReply.objects.all()
     serializer_class = TicketReplySerializer
-#----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
 
-
-#Announcement APIs ----------------------------------------------------------------
-class AnnouncementListCreateView(generics.ListCreateAPIView):
+# Announcement APIs ----------------------------------------------------------------
+class AnnouncementListCreateView(PerformCreateMixin, generics.ListCreateAPIView):
     queryset = Announcement.objects.all()
     serializer_class = AnnouncementSerializer
-    permission_classes = [AllowAny, ]
-
-    def perform_create(self, serializer):
-        serializer.save(author=Profile.objects.get(user_id=self.request.user.id))
 
     def get_permissions(self):
-        if self.request.method in SAFE_METHODS:
-            return [AllowAny(),]
-        elif self.request.method in NOT_SAFE_METHODS:
-            return  [IsAdmin(), IsAuthenticated() ]
+        return get_custom_permissions(self.request.method)
 
 class AnnouncementRetrieveUpdateView(generics.RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated, IsAdmin]
     queryset = Announcement.objects.all()
     serializer_class = AnnouncementSerializer
-#----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------
