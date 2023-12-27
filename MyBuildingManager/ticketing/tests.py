bc30339b4452dee00db79382b0f1f494e6703f8c
MohammadRezaAsgari <m.asgari1589@gmail.com>
2023-12-27 10:36:33 +0330

    writing test for tickets and announcements APIs

--- MyBuildingManager/ticketing/tests.py
+++ MyBuildingManager/ticketing/tests.py
@@ -1,3 +1,83 @@
 from django.test import TestCase
+from rest_framework_simplejwt.tokens import AccessToken
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Announcement,Ticket,TicketReply,TicketStatus
+from myauth.models import Profile
+from .serializers import AnnouncementSerializer,TicketReplySerializer,TicketSerializer
 
-# Create your tests here.
+class AnnouncementListCreateViewTest(APITestCase):
+    def setUp(self):
+        self.data = {
+            'title': 'Test Announcement',
+            'description': 'This is a test announcement',
+        }
+        # Creating a user and token
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        Profile.objects.create(user=user, is_admin=True,phone_number=0,house_number=0,floor_number=0)
+        token = AccessToken.for_user(user)
+        
+        # Sending post request with token in headers
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+        self.response = self.client.post(reverse('announcement-list'), self.data)
+
+    def test_create_announcement(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Announcement.objects.count(), 1)
+        self.assertEqual(Announcement.objects.get().title, 'Test Announcement')
+        self.assertEqual(Announcement.objects.get().description, 'This is a test announcement')
+
+    def test_get_announcement(self):
+        self.response = self.client.get(reverse('announcement-list'))
+        announcements = Announcement.objects.all()
+        serializer = AnnouncementSerializer(announcements, many=True)
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.response.data, serializer.data)
+
+class TicketListCreateViewTest(APITestCase):
+    def setUp(self):
+        self.data = {
+            'title': 'Test Ticket',
+            'description': 'This is a test ticket',
+        }
+        self.replydata = {
+            'description': 'This is a test reply',
+            'parent_ticket': 1, # Assuming there is a ticket with id=1
+        }
+        # Creating a user and token
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        Profile.objects.create(user=user, is_admin=True,phone_number=0,house_number=0,floor_number=0)
+        token = AccessToken.for_user(user)
+        
+        # Sending post request with token in headers
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+        self.response = self.client.post(reverse('ticket-list'), self.data)
+        self.response2 = self.client.post(reverse('reply-list'), self.replydata)
+
+    def test_create_ticket(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Ticket.objects.count(), 1)
+        self.assertEqual(Ticket.objects.get().title, 'Test Ticket')
+        self.assertEqual(Ticket.objects.get().description, 'This is a test ticket')
+
+    def test_get_ticket(self):
+        self.response = self.client.get(reverse('ticket-list'))
+        tickets = Ticket.objects.all()
+        serializer = TicketSerializer(tickets, many=True)
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.response.data, serializer.data)
+
+        
+    def test_create_reply(self):
+        self.assertEqual(self.response2.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(TicketReply.objects.count(), 1)
+        self.assertEqual(TicketReply.objects.get().description, 'This is a test reply')
+
+    def test_get_reply(self):
+        self.response3 = self.client.get(reverse('reply-list'))
+        replies = TicketReply.objects.all()
+        serializer = TicketReplySerializer(replies, many=True)
+        self.assertEqual(self.response3.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.response3.data, serializer.data)
--- MyBuildingManager/ticketing/urls.py
+++ MyBuildingManager/ticketing/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('tickets/', TicketListCreateView.as_view()),
+    path('tickets/', TicketListCreateView.as_view(),name='ticket-list'),
     path('tickets/<int:pk>', TicketRetrieveUpdateView.as_view()),
 
-    path('replies/', ReplyListCreateView.as_view()),
+    path('replies/', ReplyListCreateView.as_view(),name='reply-list'),
     path('replies/<int:pk>', ReplyRetrieveUpdateView.as_view()),
 
-    path('announcements/', AnnouncementListCreateView.as_view()),
+    path('announcements/', AnnouncementListCreateView.as_view(),name='announcement-list'),
     path('announcements/<int:pk>', AnnouncementRetrieveUpdateView.as_view()),
 ]
