9265322609f979a426bb9d7c9257d0fcc8f3b834
MohammadRezaAsgari <m.asgari1589@gmail.com>
2023-12-27 10:57:10 +0330

    refactored myauth serializer and documented

--- MyBuildingManager/billing/models.py
+++ MyBuildingManager/billing/models.py
@@ -3,36 +3,40 @@ from myauth.models import Profile
 from django.dispatch import receiver
 import os
 
-
 class Bill(models.Model):
-    user = models.ForeignKey(Profile,on_delete=models.CASCADE)
-    water = models.PositiveIntegerField()
-    electric = models.PositiveIntegerField()
-    gas = models.PositiveIntegerField()
-    charge = models.PositiveIntegerField()
-    others = models.PositiveIntegerField()
-    details = models.TextField()
-    payment_status = models.BooleanField(default=False)
-    receipt_photo  = models.FileField(upload_to='uploads/receipt_photo/',null=True,blank=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    water = models.PositiveIntegerField()
+    electric = models.PositiveIntegerField()
+    gas = models.PositiveIntegerField()
+    charge = models.PositiveIntegerField()
+    others = models.PositiveIntegerField()
+    details = models.TextField()
+    payment_status = models.BooleanField(default=False)
+    receipt_photo = models.FileField(upload_to='uploads/receipt_photo/', null=True, blank=True)
 
+class FileHandler:
+    """
+    Handles file operations, encapsulating the logic for deleting files.
+    This makes the code more modular and adheres to the Single Responsibility Principle.
+    """
+    @staticmethod
+    def delete_file(file_path):
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
 @receiver(models.signals.post_delete, sender=Bill)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-    if instance.receipt_photo:
-        if os.path.isfile(instance.receipt_photo.path):
-            os.remove(instance.receipt_photo.path)
+def auto_delete_file_on_delete(sender, instance, kwargs):
+    if instance.receipt_photo:
+        FileHandler.delete_file(instance.receipt_photo.path)
 
 @receiver(models.signals.pre_save, sender=Bill)
-def auto_delete_file_on_change(sender, instance, **kwargs):
-    if not instance.pk:
-        return False
+def auto_delete_file_on_change(sender, instance, kwargs):
+    if not instance.pk:
+        return False
 
-    try:
-        old_file = Bill.objects.get(pk=instance.pk).receipt_photo
-        new_file = instance.receipt_photo
-        if not old_file == new_file:
-            if os.path.isfile(old_file.path):
-                os.remove(old_file.path)
-
-    except:
-        return False
+    try:
+        old_file = Bill.objects.get(pk=instance.pk).receipt_photo
+        if old_file and old_file != instance.receipt_photo:
+            FileHandler.delete_file(old_file.path)
+    except Bill.DoesNotExist:
+        return False
--- MyBuildingManager/myauth/serializers.py
+++ MyBuildingManager/myauth/serializers.py
@@ -1,54 +1,77 @@
 from rest_framework import serializers
-from django.contrib.auth import password_validation
 from django.contrib.auth.models import User
 from .models import Profile
 
+# Abstract Factory
+class SerializerFactory:
+    def create_serializer(self, *args, kwargs):
+        raise NotImplementedError("Subclass must implement create_serializer method")
 
+# Concrete Factory for Register
+class RegisterSerializerFactory(SerializerFactory):
+    def create_serializer(self, *args, kwargs):
+        return RegisterSerializer(*args, kwargs)
+
+# Concrete Factory for User
+class UserSerializerFactory(SerializerFactory):
+    def create_serializer(self, *args, kwargs):
+        return UserSerializer(*args, kwargs)
+
+# Concrete Factory for Profile
+class ProfileSerializerFactory(SerializerFactory):
+    def create_serializer(self, *args, kwargs):
+        return ProfileSerializer(*args, **kwargs)
+
+# RegisterSerializer Class
 class RegisterSerializer(serializers.Serializer):
-    first_name = serializers.CharField(required=True, max_length=50)
-    last_name = serializers.CharField(required=True, max_length=50)
-    username = serializers.CharField(required=True,max_length=50)
-    password = serializers.CharField(
-        write_only=True,
-        required=True,
-        max_length=50,
-    )
+    first_name = serializers.CharField(required=True, max_length=50)
+    last_name = serializers.CharField(required=True, max_length=50)
+    username = serializers.CharField(required=True, max_length=50)
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        max_length=50,
+    )
+    phone_number = serializers.CharField(required=True, max_length=50)
+    house_number = serializers.IntegerField()
+    floor_number = serializers.IntegerField()
+    is_admin = serializers.BooleanField(required=False, default=False)
 
-    phone_number = serializers.CharField(required=True, max_length=50)
-    house_number = serializers.IntegerField()
-    floor_number = serializers.IntegerField()
-    is_admin = serializers.BooleanField(required=False,default=False)
-    
-    def validate(self, attrs):
-        if User.objects.filter(username=attrs["username"]).exists():
-            raise serializers.ValidationError("This username already exists!")
-        return attrs
-    
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data["username"],
-            password=validated_data["password"],
-            first_name=validated_data["first_name"],
-            last_name=validated_data["last_name"],
-        )
-        Profile.objects.create(
-            user=user,
-            phone_number=validated_data["phone_number"],
-            house_number=validated_data["house_number"],
-            floor_number=validated_data["floor_number"],
-            is_admin=validated_data["is_admin"]
-        )
-        return self.validated_data
+    def validate(self, attrs):
+        if User.objects.filter(username=attrs["username"]).exists():
+            raise serializers.ValidationError("This username already exists!")
+        return attrs
 
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            password=validated_data["password"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+        )
+        Profile.objects.create(
+            user=user,
+            phone_number=validated_data["phone_number"],
+            house_number=validated_data["house_number"],
+            floor_number=validated_data["floor_number"],
+            is_admin=validated_data["is_admin"]
+        )
+        return self.validated_data
+
+# UserSerializer Class
 class UserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = User
+        fields = ['first_name', 'last_name', 'username']
 
-    class Meta: 
-        model = User
-        fields = ['first_name','last_name','username']
-
-
+# ProfileSerializer Class
 class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    class Meta: 
-        model = Profile
-        fields = '__all__'
+    user = UserSerializer()
+
+    class Meta: 
+        model = Profile
+        fields = 'all'
+
+# Example Usage
+# factory = RegisterSerializerFactory()
+# register_serializer = factory.create_serializer()
